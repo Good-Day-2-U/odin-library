2f39acd862ed92397f18ed29c88f3fe90cabaeff
Brandon <143155030+Good-Day-2-U@users.noreply.github.com>
2023-11-01 19:04:14 -0700

    Completed the engine of adding and deleteing books More styling is need to make it look nice

--- libScript.js
+++ libScript.js
@@ -17,20 +17,34 @@ function Book(title, author, pages, read) {
 
 // console.log(TheHobbit.information())
 // console.log(JensBookAboutFey.information())
+const delSelf = function deleteSelf(button) {
+  let parentDiv = button.parentNode
+  parentDiv.remove();
+}
+
 
 let myLibrary = []
 
 const updateLibrary = function()
   {
+    const pdiv = document.querySelector('#content')
+    const childDivs = pdiv.querySelectorAll('.ImABook')
+    childDivs.forEach(div => {
+      div.remove();
+    })
 
     for (let i = 0; i < myLibrary.length; i++) {
       const newBook = document.createElement('Book' + i)
       const bookContent = document.createTextNode(myLibrary[i].information())
+      const delButton = document.createElement('Button')
+      delButton.innerHTML = 'Delete'
+      delButton.onclick = function() {delSelf(this);};
       newBook.appendChild(bookContent);
+      newBook.appendChild(delButton);
       newBook.classList.add('ImABook')
       const currentBook = document.getElementById('Book' + i)
       document.body.insertBefore(newBook, currentBook)
-      const pdiv = document.querySelector('#content')
+      // const pdiv = document.querySelector('#content')
       pdiv.append(newBook)
     }
 }
@@ -50,6 +64,9 @@ function addBookToLibrary() {
   myLibrary.push(newestBook)
   console.log(myLibrary)
   updateLibrary()
+  document.querySelector('#title').value = ''
+  document.querySelector('#author').value = ''
+  document.querySelector('#pages').value = ''
 }
 
 // updateLibrary()
