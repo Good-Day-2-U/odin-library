3b941de37119cf7f66dbabad13b316d6f771b42d
Brandon <143155030+Good-Day-2-U@users.noreply.github.com>
2023-11-01 17:53:52 -0700

    Need to fix the array stacking elements onto eachother Need to add delete button to new elements

--- libScript.js
+++ libScript.js
@@ -12,27 +12,44 @@ function Book(title, author, pages, read) {
 }
 
 
-const TheHobbit = new Book('The Hobbit', 'J.R.R. Toilkin', 556, false)
-const JensBookAboutFey = new Book('Fey Mist Kingdom', 'Casandra Clair', 7643, true)
+// const TheHobbit = new Book('The Hobbit', 'J.R.R. Toilkin', 556, false)
+// const JensBookAboutFey = new Book('Fey Mist Kingdom', 'Casandra Clair', 7643, true)
 
-console.log(TheHobbit.information())
-console.log(JensBookAboutFey.information())
+// console.log(TheHobbit.information())
+// console.log(JensBookAboutFey.information())
 
-const myLibrary = [TheHobbit, JensBookAboutFey]
+let myLibrary = []
 
-for (let i = 0; i < myLibrary.length; i++) {
-  const newBook = document.createElement('Book' + i)
-  const bookContent = document.createTextNode(myLibrary[i].information())
-  newBook.appendChild(bookContent);
-  newBook.classList.add('ImABook')
-  const currentBook = document.getElementById('Book' + i)
-  document.body.insertBefore(newBook, currentBook)
-  const pdiv = document.querySelector('#content')
-  pdiv.append(newBook)
+const updateLibrary = function()
+  {
 
+    for (let i = 0; i < myLibrary.length; i++) {
+      const newBook = document.createElement('Book' + i)
+      const bookContent = document.createTextNode(myLibrary[i].information())
+      newBook.appendChild(bookContent);
+      newBook.classList.add('ImABook')
+      const currentBook = document.getElementById('Book' + i)
+      document.body.insertBefore(newBook, currentBook)
+      const pdiv = document.querySelector('#content')
+      pdiv.append(newBook)
+    }
 }
 
 
 function addBookToLibrary() {
-  
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read;
+  if (document.querySelector('#true').checked == true) {
+    read = true
+  } else {
+    read = false
+  }
+  let newestBook = new Book(title, author, pages, read)
+  myLibrary.push(newestBook)
+  console.log(myLibrary)
+  updateLibrary()
 }
+
+// updateLibrary()
